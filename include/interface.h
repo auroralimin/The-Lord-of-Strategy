4b420e175512f32164eb8cb36ee13a7f333dea04
auroralimin <auroralimin@hotmail.com>
2014-06-20 20:28:39 -0300

    Atualizacao do modulo de interface.
    O modulo de interface agora contem funcoes que inicializam com mais detalhes a
    interface e que podem gerar/deletar WINDOWs.

--- include/interface.h
+++ include/interface.h
@@ -1,2 +1,5 @@
+void init_interface();
 void get_dimension();
+WINDOW *create_win(int win_y, int win_x, int starty, int startx);
+void destroy_win(WINDOW **local_win);
 void printw_map();
--- src/interface.c
+++ src/interface.c
@@ -4,6 +4,17 @@
 #include "basis.h"
 #include "interface.h"
 
+void init_interface()
+{
+	initscr();
+	cbreak();
+	start_color();
+	noecho();
+	curs_set(0);
+	keypad(stdscr, TRUE);
+	get_dimension();
+}
+
 /* alerta o usuario se a janela do terminal nao tiver altura suficiente
 para o jogo */
 void get_dimension()
@@ -17,6 +28,27 @@ void get_dimension()
 		printf("We suggest you use a smaller font.\n");
 		exit (1);
 	}
+}
+
+WINDOW *create_win(int win_y, int win_x, int starty, int startx)
+{
+	WINDOW *local_win;
+
+	refresh();
+	local_win = newwin(win_y, win_x, starty, startx);
+	box(local_win, 0 , 0);
+	wrefresh(local_win);
+	refresh();
+
+	return local_win;
+}
+
+void destroy_win(WINDOW **local_win)
+{
+	werase(*local_win);
+	wrefresh(*local_win);
+	delwin(*local_win);
+	*local_win = NULL;
 }
 
 /* printa o mapa na janela do terminal */
