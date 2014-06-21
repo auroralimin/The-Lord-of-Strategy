d560fad4c43802b3882b7d74c4e72b61c1e1a2c1
annimus <guilherme.ferreira.silva9@gmail.com>
2014-06-21 20:11:15 -0300

    Adicao de mais testes no modulo CUnit.
    Foi adicionado os testes unitarios do modulo logico do programa.

--- src/cUnit_main.c
+++ src/cUnit_main.c
@@ -5,6 +5,8 @@
 #include "interface.h"
 #include  "CUnit/Basic.h"
 
+unit hobbit;
+
 /* suites initialization and clean fuctions and fuctions */
 int init_basis(void)
 {
@@ -17,17 +19,21 @@ int clean_basis(void)
 {
 	return 0;
 }
-/*
+
 int init_logic(void)
 {
+	aloc_map();
+	init_map();
 	return 0;
 }
 
 int clean_logic(void)
 {
+	free_map();
 	return 0;
 }
 
+/*
 int init_interface(void)
 {
 	return 0;
@@ -69,6 +75,56 @@ void aloc_options_test(void)
 }
 
 /* logic tests functions */
+void init_options_test(void)
+{
+	init_options();
+	CU_ASSERT(options != NULL);
+}
+
+void race_init_test(void)
+{
+	unit hobbit = race_init(HOBBIT);
+	CU_ASSERT(hobbit.race == HOBBIT);
+	CU_ASSERT(hobbit.hp == 100);
+	CU_ASSERT(hobbit.spd == 1);
+	CU_ASSERT(hobbit.dmg == 10);
+}
+
+void load_build_test(void)
+{
+	CU_ASSERT((load_build("ASCII art/frodo_house.txt",
+	FRODO_ROW, FRODO_COL) == 1);
+}
+
+void get_art_test(void)
+{
+	CU_ASSERT((get_art()) == 1);
+}
+
+void print_unit_test(void)
+{
+	printmap_unit(hobbit);
+	CU_ASSERT(map[hobbit.position[0]+9][hobbit.position[1]] != ' ');
+}
+
+void clear_unit_test(void)
+{
+	int aux_x = hobbit.position[1], aux_y = hobbit.position[0] + 9;
+
+	clear_unit(hobbit);
+	CU_ASSERT(map[aux_y][aux_x] == ' ');
+}
+
+void move_unit_test(void)
+{
+	int aux;
+
+	aux = hobbit.position[1];
+	move_unit(hobbit);
+
+	CU_ASSERT(hobbit.position[0] == size_row - hobbit.height);
+	CU_ASSERT(hobbit.position[1] == aux + 9);
+}
 
 /* interface tests functions */
 
@@ -99,10 +155,20 @@ int main()
 
 
 	/* add Logic suite to the registry */
-	/*pSuite_logic = CU_add_suite("Suite: Logic", init_logic, clean_logic);*/
+	pSuite_logic = CU_add_suite("Suite: Logic", init_logic, clean_logic);
 
-	/* add tests to the suite */
-
+	/* add tests to the Logic suite */
+	if ((NULL==CU_add_test(pSuite_logic, "init_options", init_options_test))
+	   ||(NULL==CU_add_test(pSuite_logic, "race_init", race_init_test))
+	   ||(NULL==CU_add_test(pSuite_logic, "load_build", load_build_test))
+	   ||(NULL==CU_add_test(pSuite_logic, "get_art", get_art_test))
+	   ||(NULL==CU_add_test(pSuite_logic, "printmap_unit",  print_unit_test))
+	   ||(NULL==CU_add_test(pSuite_logic, "clear_unit", clear_unit_test))
+	   ||(NULL==CU_add_test(pSuite_logic, "move_unit", move_unit_test)))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
 
 
 	/* add Interface suite to the registry */
