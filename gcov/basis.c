9cfeed08518aa38b14c1c197e40f570021f9ebfd
auroralimin <auroralimin@hotmail.com>
2014-06-30 20:13:46 -0300

    Criacao dos testes de cobertura.
    Foi criada uma pasta gcov que contem todos os codigos fontes e uma pasta com as
    artes que o jogo precisa para ser compilado e executado propriamente. Isso
    deve-se ao fato de que, para os testes funcionarem, alguns arquivos tem que
    estar na mesma pasta. Alem disso, foi criado um makefile com dois targets
    necessarios para realizar os testes de cobertura.
    
    Para ter o resultado dos testes e' preciso dar "make game", que compila todos os
    codigos-fonte com os parametros do gcov e faz o link dos objetos (gerando um
    executavel "game" que e' monitorado pelo gcov. Depois disso, deve-se executar o
    binario gerado e testar a maior quantidade funcionalidades do jogo possivel.
    Apos o termino da execucao, deve-se fazer o comando "make gcovtest" para ter
    acesso aos resultados dos testes de cobertura.
    
    Para informacoes mais detalhadas, deve-se visualizar os arquivos *.gcov gerados
    apos o processo descrito.

---
+++ gcov/basis.c
@@ -0,0 +1,207 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include "basis.h"
+#include "logic.h"
+
+pthread_mutex_t l_key, l_scroll;
+char **map = NULL;
+char **options[N_OPTIONS];
+
+pthread_t key_thread;
+int options_len[] = { NEW_GAME, LOAD_GAME, EXIT_GAME};
+
+void init_locks()
+{
+	pthread_mutex_init(&l_key, NULL);
+	pthread_mutex_init(&l_scroll, NULL);
+}
+
+void init_thread()
+{
+	if (pthread_create(&key_thread, NULL, &read_key, NULL))
+	{
+		endwin();
+		fprintf(stderr, "Cannot create thread read_key\n");
+		exit(-1);
+	}
+}
+
+void prepare_map()
+{
+	aloc_map();
+	init_map();
+	get_art();
+	put_builds();
+	map_spaces();
+}
+
+/* aloca uma matriz na memoria para guardar o mapa do jogo */
+void aloc_map()
+{
+	int i;
+
+	map = (char**) calloc(MAP_ROW, sizeof(char*));
+	for (i = 0; i < MAP_ROW; i++)
+		map[i] = (char*) calloc(MAP_COL, sizeof(char));
+}
+
+/* inicializa o mapa com espaços */
+void init_map()
+{
+	int i, j;
+
+	for (i = 0; i < MAP_ROW; i++)
+		for (j = 0; j < MAP_COL; j++)
+                   map[i][j] = ' ';
+}
+
+/* libera o mapa */
+void free_map()
+{
+	int i;
+
+	if (map != NULL)
+	{
+		for (i = 0; i < MAP_ROW; i++)
+			if (map[i] != NULL)
+				free(map[i]);
+		free(map);
+		map = NULL;
+	}
+}
+
+/* le os arquivos .txt contendo a ascii art do jogo */
+FILE* read_file(char *name)
+{
+	FILE *fp = NULL;
+
+	fp = fopen(name, "r");
+	if (fp == NULL)
+	{
+		endwin();
+		printf("%s not found\n", name);
+		free_build();
+		free_map();
+		exit(1);
+	}
+
+	return fp;
+}
+
+void aloc_options()
+{
+	int i, j;
+
+	for (i = 0; i < N_OPTIONS; i++)
+	{
+		options[i] = (char**) calloc(OPTIONS_WIDTH, sizeof(char*));
+		for (j = 0; j < OPTIONS_WIDTH; j++)
+			options[i][j] = (char*)
+			calloc(options_len[i], sizeof(char));
+	}
+}
+
+void free_options()
+{
+	int i, j;
+
+	for (i = 0; i < N_OPTIONS; i++){
+		for (j = 0; j < OPTIONS_WIDTH; j++)
+		{
+			if (options[i][j] != NULL)
+				free(options[i][j]);
+		}
+		if (options[i] != NULL)
+		{
+			free(options[i]);
+			options[i] = NULL;
+		}
+	}
+}
+
+int insert_unit(unit **top, int race)
+{
+	unit *new = (unit*) calloc(1, sizeof(unit));
+
+	if (new == NULL)
+		return -1;
+
+	race_init(new, race);
+
+	if (*top == NULL)
+	{
+		*top = new;
+		return 1;
+	}
+
+	new->next = *top;
+	*top = new;
+
+	return 1;
+}
+
+void free_units(unit **top)
+{
+	unit *aux;
+
+	while (aux != NULL)
+	{
+		aux = aux->next;
+		free(*top);
+		*top = aux;
+	}
+
+	*top = NULL;
+}
+
+void create_listbuild()
+{
+	int i;
+
+	for (i = 3; i >= 0; i--)
+		if (!insert_build(i))
+		{
+			endwin();
+			printf("Insufficient memory\n");
+			exit(1);
+		}
+}
+
+int insert_build(int id)
+{
+	build *build_top = get_buildtop();
+	build *new = (build*) calloc(1, sizeof(build));
+
+	if (new == NULL)
+		return -1;
+
+	new->id = id;
+	new->level = 1;
+	new->storage = 0;
+	new->income = 50;
+
+	if (build_top == NULL)
+	{
+		set_buildtop(new);
+		return 1;
+	}
+
+	new->next = build_top;
+	build_top = new;
+	set_buildtop(build_top);
+
+	return 1;
+}
+
+void free_build()
+{
+	build *build_top = get_buildtop();
+	build *aux = build_top;
+
+	while (aux != NULL) {
+		aux = aux->next;
+		free(build_top);
+		build_top = aux;
+	}
+	set_buildtop(NULL);
+}
---
+++ gcov/logic.c
@@ -0,0 +1,597 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <pthread.h>
+#include <math.h>
+#include "basis.h"
+#include "logic.h"
+#include "interface.h"
+
+struct build_art
+{
+	char name[50];
+	int row, col;
+};
+
+static char *options_files[] = { "ASCII art/new_game.txt",
+			"ASCII art/load_game.txt",
+			"ASCII art/exit.txt"};
+
+
+static struct build_art builds[]={
+{"ASCII art/house_frodo.txt", MAP_ROW - FRODO_ROW, FRODO_COL},
+{"ASCII art/mordor_tower.txt",  MAP_ROW - MORDOR_ROW, MORDOR_COL},
+{"ASCII art/gold_build.txt", 1,  GOLD_COL1},
+{"ASCII art/food_build.txt", 1, FOOD_COL1},
+{"ASCII art/wood_build.txt", 1, WOOD_COL1},
+{"ASCII art/metal_build.txt", 1, METAL_COL1}
+};
+
+/* matriz contendo o endereço da ascii art do jogo */
+static char *name_filearts[] = { "ASCII art/hobbit.txt",
+			         "ASCII art/elf.txt",
+				 "ASCII art/dwarf.txt",
+				 "ASCII art/ent.txt",
+				 "ASCII art/goblin.txt",
+				 "ASCII art/orc.txt",
+				 "ASCII art/warg.txt",
+				 "ASCII art/troll.txt"
+};
+
+char *options_frodo[] = { "ASCII art/house_option0.txt",
+		          "ASCII art/house_option1.txt",
+		          "ASCII art/house_option2.txt",
+		          "ASCII art/house_option3.txt",
+		          "ASCII art/house_option4.txt"
+};
+
+static unit attr[] = {
+{HOBBIT, 100, 1, 10, 10, {30, 40}, {30, 40}, 100, {0, 0, 0, 0}, NULL},
+{ELF, 130, 4, 40, 12, {28, 40}, {28, 40}, 200,  {0, 0, 0, 0}, NULL},
+{DWARF, 250, 2, 30, 10, {30, 40}, {30, 40}, 250,  {0, 0, 0, 0}, NULL},
+{ENT, 500, 1, 50, 15, {25, 40}, {25, 40}, 300,  {0, 0, 0, 0}, NULL},
+{GOBLIN, 100, 1, 10, 10, {30, 34+MORDOR_COL}, {30, 34+MORDOR_COL}, 100,
+{0, 0, 0, 0}, NULL},
+{ORC, 200, 2, 30, 12, {28, 34+MORDOR_COL}, {28, 34+MORDOR_COL}, 200,
+{0, 0, 0, 0}, NULL},
+{WARG, 120, 5, 25, 8, {31, 34+MORDOR_COL}, {31, 34+MORDOR_COL}, 250,
+{0, 0, 0, 0}, NULL},
+{TROLL, 500, 1, 50, 15, {25, 34+MORDOR_COL}, {25, 34+MORDOR_COL}, 300,
+{0, 0, 0, 0}, NULL}
+};
+
+static int good_col[] = {HOBBIT_GOLD, HOBBIT_FOOD, HOBBIT_WOOD, HOBBIT_METAL};
+
+static int prices[4][4] = {{500, 500, 0, 0},
+	                  {800, 800, 0, 1500},
+		          {1000, 1500, 0, 500},
+		          {1500, 1500, 3000, 0}
+};
+
+scrll map_scroll;
+int status[2];
+int lim_map = 0;
+int term_col = 1;
+char mat_races[N_RACES + 1][RACE_HEIGHT][RACE_WIDTH];
+MEVENT event;
+build *build_top = NULL;
+unit *free_races = NULL;
+fortress frodo_house = {1, 15000, 1, 0};
+player user = {10000, 10000, 10000, 10000, 10000};
+
+void* read_key(void *arg)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&l_key);
+		switch(getch())
+		{
+	 		case (KEY_MOUSE):
+			if(getmouse(&event) == OK)
+			{
+				status[0] = STATUS_MOUSE_MOVED;
+				if (event.bstate == BUTTON1_CLICKED)
+				{
+					status[0] = STATUS_MOUSE_CLICK;
+					if ((event.y >= map_scroll.row-1) &&
+					   (event.y <= map_scroll.row+1))
+						mouse_scroll(event.x);
+					else
+						click_frodooption();
+				}
+			}
+			break;
+
+			case (KEY_UP):
+			status[0] = STATUS_UP;
+			break;
+
+			case (KEY_DOWN):
+			status[0] = STATUS_DOWN;
+			break;
+
+			case (KEY_RIGHT):
+			if (status[1] != STATUS_MENU)
+				arrow_scroll(SCROLL_RIGHT);
+			break;
+
+			case (KEY_LEFT):
+			if (status[1] != STATUS_MENU)
+				arrow_scroll(SCROLL_LEFT);
+			break;
+
+			case (EXIT):
+			free_build();
+			free_map();
+			endwin();
+			exit(1);
+
+			case (ENTER):
+			status[0] = STATUS_ENTER;
+		}
+		pthread_mutex_unlock(&l_key);
+		usleep(2);
+	}
+
+	return arg;
+}
+
+player get_user()
+{
+	return user;
+}
+
+MEVENT get_event()
+{
+	return event;
+}
+
+int get_keystatus()
+{
+	return status[0];
+}
+
+void set_keystatus(int key_status)
+{
+	status[0] = key_status;
+}
+
+int get_gamestatus()
+{
+	return status[1];
+}
+
+void set_gamestatus(int game_status)
+{
+	status[1] = game_status;
+}
+
+int get_termcol()
+{
+	return term_col;
+}
+
+void set_maplim(int lim)
+{
+	lim_map = lim;
+}
+
+void init_options()
+{
+	int i, j;
+	FILE *fp = NULL;
+
+	for (i = 0; i < N_OPTIONS; i++)
+	{
+		fp = read_file(options_files[i]);
+		for (j = 0; j < OPTIONS_WIDTH; j++)
+		{
+			fscanf(fp, "%[^\n]s", options[i][j]);
+			if (feof(fp))
+				break;
+			fgetc(fp);
+		}
+		fclose(fp);
+	}
+}
+
+int report_option(int mouse_row, int mouse_col)
+{
+	if ((mouse_row > 1) && (mouse_row < MENU_ROW - 2) &&
+	   (mouse_col > 1) && (mouse_col < MENU_COL - 2))
+		return 1 + (mouse_row/ 13);
+	return 0;
+}
+
+void mouse_scroll(int mouse_col)
+{
+	if ((mouse_col > 0) && (mouse_col <= map_scroll.col))
+	{
+		pthread_mutex_lock(&l_scroll);
+		map_scroll.position = mouse_col;
+		map_scroll.residue = 0;
+		term_col = map_scroll.proportion * (map_scroll.position - 1);
+		term_coltest();
+		pthread_mutex_unlock(&l_scroll);
+		wprintw_map();
+	}
+}
+
+void arrow_scroll(int direction)
+{
+	int residue;
+
+	if ((term_col <= lim_map) && (MAP_COL > get_sizecol()) &&
+	   (term_col >= 0))
+	{
+		pthread_mutex_lock(&l_scroll);
+		term_col+=direction;
+		map_scroll.residue+=sqrt(pow(direction, 2));
+		if (map_scroll.residue > map_scroll.proportion)
+		{
+			residue = map_scroll.residue / map_scroll.proportion;
+			if (direction > 0)
+				map_scroll.position+=residue;
+			else
+				map_scroll.position-=residue;
+			map_scroll.residue-=map_scroll.proportion*residue;
+		}
+		term_coltest();
+		pthread_mutex_unlock(&l_scroll);
+		wprintw_map();
+	}
+}
+
+scrll get_mapscroll()
+{
+	return map_scroll;
+}
+
+void set_mapscroll(scrll scroll_map)
+{
+	map_scroll = scroll_map;
+}
+
+void term_coltest()
+{
+	if (term_col < 0)
+	{
+		term_col = 0;
+		map_scroll.position = 1;
+		map_scroll.residue = 0;
+	}
+	if (term_col > lim_map)
+	{
+		term_col = lim_map;
+		map_scroll.position = map_scroll.col;
+		map_scroll.position-=map_scroll.col / map_scroll.proportion;
+		map_scroll.residue = 0;
+	}
+}
+
+/* inicializa as unidades com os atributos pre-definidos */
+void race_init(unit *chr, int race)
+{
+	int i;
+
+	for (i = 0; i < N_RACES; i++)
+		if (attr[i].race == race)
+		{
+			chr->race = attr[i].race;
+			chr->hp = attr[i].hp;
+			chr->spd = attr[i].spd;
+			chr->dmg = attr[i].dmg;
+			chr->height = attr[i].height;
+			chr->position[0] = attr[i].position[0];
+			chr->position[1] = attr[i].position[1];
+			chr->destination[0] = attr[i].destination[0];
+			chr->destination[1] = attr[i].destination[1];
+			chr->spwan_time = attr[i].spwan_time;
+			break;
+		}
+}
+
+/* carrega o mapa com as fortalezas do jogador e do computador */
+int load_build(char *file_name, int art_row, int art_col)
+{
+	int i;
+	FILE *fp = NULL;
+
+	fp = read_file(file_name);
+
+	for (i = art_row; i < MAP_ROW; i++)
+	{
+		fscanf(fp, "%[^\n]s", map[i] + art_col);
+		fgetc(fp);
+		if (feof(fp))
+			break;
+	}
+	fclose(fp);
+
+	return 1;
+}
+
+void put_builds()
+{
+	int i;
+
+	for (i = 0; i < N_BUILDS; i++)
+		load_build(builds[i].name, builds[i].row, builds[i].col);
+}
+
+/* le a ascii art das racas */
+int get_art()
+{
+	int i, j;
+	FILE *fp = NULL;
+
+	for (i = 0; i < N_RACES; i++)
+	{
+		fp = read_file(name_filearts[i]);
+		for (j = 0; j < 15; j++)
+		{
+			fscanf(fp, "%[^\n]s", mat_races[i][j]);
+			fgetc(fp);
+			if (feof(fp))
+				break;
+		}
+		fclose(fp);
+	}
+
+	return 1;
+}
+
+void map_spaces()
+{
+	int i, j;
+
+	for (i = 0; i < MAP_ROW; i++)
+		for (j = 0; j < MAP_COL; j++)
+			if (map[i][j] == '\0')
+				map[i][j] = ' ';
+}
+
+/* printa as unidades na tela do terminal */
+void printmap_unit(unit chr)
+{
+	int i, j, row = chr.position[0], col = chr.position[1];
+
+	for (i = RACE_HEIGHT - chr.height; i < RACE_HEIGHT; i++)
+	{
+		for (j = 0; j < RACE_WIDTH; j++)
+		{
+			if (mat_races[chr.race-1][i][j] != ' ')
+				map[row][col] = mat_races[chr.race-1][i][j];
+			col++;
+		}
+		col = chr.position[1];
+		row++;
+	}
+}
+
+void clear_unit(unit chr)
+{
+	int i, j, row = chr.position[0], col = chr.position[1];
+
+	for (i = RACE_HEIGHT - chr.height; i < RACE_HEIGHT; i++)
+	{
+		for (j = 0; j < RACE_WIDTH; j++)
+			map[row][col++] = ' ';
+		col = chr.position[1];
+		row++;
+	}
+
+	if (col < FRODO_WIDTH)
+		load_build(builds[0].name, builds[0].row, builds[0].col);
+	else if (col > MORDOR_COL)
+		load_build(builds[1].name, builds[1].row, builds[1].col);
+	map_spaces();
+}
+
+void move_unit(unit *chr)
+{
+	int dest = 1, n = 1;
+
+	if (chr->race > 4)
+		dest = -1;
+	if ((chr->position[1]*dest >= chr->destination[1]*dest) &&
+	   (chr->position[0] != chr->destination[0]))
+	{
+		n = 0;
+	}
+
+	if (chr->position[n]*dest > chr->destination[n]*dest)
+		dest = dest * -1;
+	if (chr->position[n]*dest < chr->destination[n]*dest)
+	{
+		if (n == 0)
+			chr->position[0]+= chr->height * dest;
+		else
+			chr->position[1]+= 9 * dest;
+		if (chr->position[n]*dest > chr->destination[n]*dest)
+			chr->position[n] = chr->destination[n];
+	}
+}
+
+void good_generator()
+{
+	build *aux;
+
+	for (aux = build_top; aux != NULL; aux = aux->next)
+	{
+		if (aux->storage != pow(10, aux->level+2) / 2)
+			aux->storage+=aux->income;
+	}
+}
+
+unit* get_freeraces()
+{
+	return free_races;
+}
+
+void set_freeraces(unit *top)
+{
+	free_races = top;
+}
+
+build* get_buildtop()
+{
+	return build_top;
+}
+
+void set_buildtop(build *top)
+{
+	build_top = top;
+}
+
+void print_good()
+{
+	int i = 0, j = 0, n = 2, storage = 0, num = 0;
+	build *aux = NULL;
+
+	for (i = MAP_COL - FRODO_COL; i < MAP_COL - MORDOR_COL; i++)
+		map[0][i] = ' ';
+
+	i = 0;
+	for (aux = build_top; aux != NULL; aux = aux->next)
+	{
+		storage = aux->storage;
+
+		for (i = 0; i < 6; i++)
+			map[0][builds[n].col+i] = ' ';
+
+		for (i = 999999; i > 0; i = i/10)
+		{
+			if (storage > i)
+			{
+				num = storage/(i+1);
+				map[0][builds[n].col+j++] = num + 48;
+				storage = storage - ((i + 1) * num);
+			}
+			else if (j > 0)
+					map[0][builds[n].col+j++] = 48;
+		}
+		map[0][builds[n].col+j] = storage + 48;
+		n++;
+		j = 0;
+	}
+}
+
+void check_good(unit *chr)
+{
+	int i, j;
+	build *aux = build_top;
+
+	for (i = 0; i < N_BUILDS - 2; i++)
+	{
+		if ((chr->position[1] == good_col[i]) &&
+				(chr->position[0] == GOOD_ROW))
+		{
+			while ((aux != NULL) && (aux->id != i))
+				aux = aux->next;
+
+			if (aux->id == i)
+			{
+				chr->backpack[i]+=aux->storage;
+				aux->storage = 0;
+				chr->destination[0] = 30;
+				chr->destination[1] = 40;
+			}
+		}
+		else if ((chr->position[1] == 40) &&
+				(chr->position[0] == 30))
+		{
+			if (chr->race == HOBBIT)
+			{
+				user.gold+=chr->backpack[0];
+				user.food+=chr->backpack[1];
+				user.wood+=chr->backpack[2];
+				user.metal+=chr->backpack[3];
+				for (j = 0; j < 4; j++)
+					chr->backpack[j] = 0;
+			}
+		}
+		else
+			break;
+	}
+}
+
+void goto_build(unit *chr, int n_build)
+{
+	int i;
+
+	if (chr->race == HOBBIT)
+		for (i = 0; i < N_BUILDS - 2; i++)
+			if (i == n_build)
+			{
+				chr->destination[0] = GOOD_ROW;
+				chr->destination[1] = good_col[i];
+				break;
+			}
+}
+
+void load_houseoption(int n)
+{
+	int i, j;
+	FILE *fp = read_file(options_frodo[n]);
+
+	for (i = 0; i < 6; i++)
+	{
+		for (j = 0; j < 13; j++)
+			fscanf(fp, "%c", &map[builds[0].row-7+i][n*15+j+5]);
+		fgetc(fp);
+	}
+}
+
+void fortress_buy(int col)
+{
+	int i, option = 0;
+
+	for (i = 5; i < 80; i+=15)
+	{
+		if ((col >= i) && (col <= i+13))
+		{
+			if ((option != 0) && (frodo_house.level >= option) &&
+		   	   (user.gold >= prices[option - 1][0]) &&
+		           (user.food >= prices[option - 1][1]) &&
+		           (user.wood >= prices[option - 1][2]) &&
+		           (user.metal >= prices[option - 1][3]))
+	          	{
+		 		insert_unit(&free_races, option);
+				break;
+			}
+			else if ((option == 0) && (frodo_house.level < 4) &&
+				(user.gold >= 1000 * (frodo_house.level+1)) &&
+				(user.food >= 1000 * (frodo_house.level+1)) &&
+				(user.wood >= 1000 * (frodo_house.level+1)))
+			{
+				user.gold-=1000 * (frodo_house.level + 1);
+				user.food-=1000 * (frodo_house.level + 1);
+				user.wood-=1000 * (frodo_house.level + 1);
+				frodo_house.level++;
+				option_upgrade(frodo_house.level);
+			}
+		}
+		option++;
+	}
+}
+
+void option_upgrade(int level)
+{
+	int i, j;
+
+	if ((level > 1) && (level < 4))
+	{
+		load_houseoption(level);
+		for (i = 0; i < 3; i++)
+			map[builds[0].row-5+i][12] = level + 1 + 48;
+	}
+	else if (level == 4)
+	{
+		load_houseoption(level);
+		for (i = 0; i < 6; i++)
+			for (j = 0; j < 13; j++)
+				map[builds[0].row-7+i][5+j] = ' ';
+	}
+}
